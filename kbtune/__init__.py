db34d5945b6bb6d767d7ad631215c8deadb49de8
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2010-06-09 22:51:02 +0100

    Added Note ordering, interval ratios and cents, temperament frequencies, and deviations in cents, bps, and bpm, and tuning in fifths.

--- kbtune/__init__.py
+++ kbtune/__init__.py
@@ -14,6 +14,34 @@ import math # log
 
 __version__ = '1.0'
 
+# from http://code.activestate.com/recipes/576685/
+# we can import this from functools from Python 2.7 / 3.2 onwards
+def total_ordering(cls):
+    'Class decorator that fills-in missing ordering methods'    
+    convert = {
+        '__lt__': [('__gt__', lambda self, other: other < self),
+                   ('__le__', lambda self, other: not other < self),
+                   ('__ge__', lambda self, other: not self < other)],
+        '__le__': [('__ge__', lambda self, other: other <= self),
+                   ('__lt__', lambda self, other: not other <= self),
+                   ('__gt__', lambda self, other: not self <= other)],
+        '__gt__': [('__lt__', lambda self, other: other > self),
+                   ('__ge__', lambda self, other: not other > self),
+                   ('__le__', lambda self, other: not self > other)],
+        '__ge__': [('__le__', lambda self, other: other >= self),
+                   ('__gt__', lambda self, other: not other >= self),
+                   ('__lt__', lambda self, other: not self >= other)]
+    }
+    roots = set(dir(cls)) & set(convert)
+    assert roots, 'must define at least one ordering operation: < > <= >='
+    root = max(roots)       # prefer __lt __ to __le__ to __gt__ to __ge__
+    for opname, opfunc in convert[root]:
+        if opname not in roots:
+            opfunc.__name__ = opname
+            opfunc.__doc__ = getattr(int, opname).__doc__
+            setattr(cls, opname, opfunc)
+    return cls
+
 def ratio_to_cents(ratio):
     """Convert ratio to cents.
 
@@ -30,6 +58,7 @@ def cents_to_ratio(cents):
     """
     return 2 ** (cents / 1200)
 
+@total_ordering
 class Note:
     """Note as key with accidental."""
     KEYS = {'C': 0, 'D': 1, 'E': 2, 'F': 3, 'G': 4, 'A': 5, 'B': 6}
@@ -56,8 +85,38 @@ class Note:
                 else:
                     raise ValueError("Unknown accidental '%s'." % accidental)
         elif isinstance(key, int):
-            self.key = key
+            self.key = key % 7
             self.accidental = accidental
+
+    def __add__(self, interval):
+        """Add an interval to the note.
+
+        >>> print(Note('C') + Interval(Note('E'), Note('G')))
+        Eb
+        >>> print(Note('C') + Interval(Note('G'), Note('E')))
+        A
+        >>> print(Note('D') + Interval(Note('C'), Note('E')))
+        F#
+        """
+        direction = +1 if interval.up else -1
+        # create the new note at the right key, without accidentals for now
+        new_note = Note(self.key + interval.key_distance * direction)
+        # change the accidentals to match the desired position
+        new_note.accidental = (
+            (self.position + interval.position_distance * direction) % 12
+            - new_note.position)
+        return new_note
+
+    def __eq__(self, other):
+        return self.key == other.key and self.accidental == other.accidental
+
+    def __lt__(self, other):
+        if self.key < other.key:
+            return True
+        elif self.key == other.key:
+            return self.accidental < other.accidental
+        else:
+            return False
 
     def __repr__(self):
         """Return string representation.
@@ -123,7 +182,7 @@ class Interval:
         (2, 2): 'dim3', # diminished third
         (2, 3): 'm3',   # minor third
         (2, 4): 'M3',   # major third
-        (2, 5): 'M3',   # augmented third
+        (2, 5): 'aug3', # augmented third
         (3, 4): 'dim4', # diminished fourth
         (3, 5): 'P4',   # perfect fourth
         (3, 6): 'aug4', # augmented fourth
@@ -155,6 +214,58 @@ class Interval:
             self.key_distance = 0
             self.position_distance = abs(position_distance)
             self.up = (position_distance >= 0)
+
+    def get_ratio(self):
+        """Get the ratio of the natural frequencies of the interval.
+
+        >>> Interval(Note('E'), Note('B')).get_ratio()
+        1.5
+        >>> Interval(Note('A'), Note('E')).get_ratio()
+        0.75
+        """
+        if self.key_distance == 0 and self.position_distance == 0:
+            # perfect unison
+            ratio = 1
+        elif self.key_distance == 1 and self.position_distance == 2:
+            # major second
+            ratio = 8 / 7
+        elif self.key_distance == 2 and self.position_distance == 3:
+            # minor third
+            ratio = 6 / 5
+        elif self.key_distance == 2 and self.position_distance == 4:
+            # major third
+            ratio = 5 / 4
+        elif self.key_distance == 3 and self.position_distance == 5:
+            # perfect fourth
+            ratio = 4 / 3
+        elif self.key_distance == 3 and self.position_distance == 6:
+            # augmented fourth
+            ratio = 10 / 7
+        elif self.key_distance == 4 and self.position_distance == 6:
+            # diminished fifth
+            ratio = 7 / 5 # (is less than augmented fourth!!)
+        elif self.key_distance == 4 and self.position_distance == 7:
+            # perfect fifth
+            ratio = 3 / 2
+        elif self.key_distance == 5 and self.position_distance == 8:
+            # minor sixth
+            ratio = 8 / 5
+        elif self.key_distance == 5 and self.position_distance == 9:
+            # major sixth
+            ratio = 10 / 6
+        elif self.key_distance == 5 and self.position_distance == 9:
+            # minor seventh
+            ratio = 7 / 4
+        else:
+            raise ValueError("Not a natural interval.")
+        if self.up:
+            return ratio
+        else:
+            return 1 / ratio
+        
+
+    def get_cents(self):
+        return ratio_to_cents(self.get_ratio())
 
     def __repr__(self):
         return 
@@ -188,6 +299,8 @@ class Interval:
         +m7
         >>> print(Interval(Note("C"), Note("B")))
         +M7
+        >>> print(Interval(Note('G'), Note('E')))
+        -m3
         """
         return (("+" if self.up else "-")
                 + self.NAMES[(self.key_distance, self.position_distance)])
@@ -214,6 +327,9 @@ class Temperament:
         ]
     """The twelve notes in an octave on a keyboard."""
 
+    MAJOR_THIRD = Interval(Note('C'), Note('E'))
+    PERFECT_FIFTH = Interval(Note('C'), Note('G'))
+
     def __init__(self):
         self.frequencies = [None] * 12
         """List of frequencies for each note."""
@@ -229,6 +345,190 @@ class Temperament:
         True
         """
         return self.frequencies[position] is not None
+
+    def set_frequency(self, note, freq):
+        """Tune a note at a particular frequency.
+
+        >>> temp = Temperament()
+        >>> temp.set_frequency(Note('A'), 415)
+        >>> temp.frequencies
+        [None, None, None, None, None, None, None, None, None, 415, None, None]
+        """
+        self.frequencies[note.position] = freq
+
+    def get_frequency(self, note):
+        """Get frequency of a note.
+
+        >>> temp = Temperament()
+        >>> temp.set_frequency(Note('G#'), 400)
+        >>> temp.get_frequency(Note('Ab'))
+        400
+        """
+        return self.frequencies[note.position]
+
+    def get_cents(self, note1, note2):
+        """Get the (signed) size of the interval in cents."""
+        return ratio_to_cents(
+            self.get_frequency(note2)
+            / self.get_frequency(note1))
+
+    def get_dev_cents(self, note1, note2):
+        """Get the deviation of the interval in cents."""
+        return (
+            self.get_cents(note1, note2)
+            - Interval(note1, note2).get_cents())
+
+    def get_dev_bps(self, note1, note2):
+        """Get the difference of the first common harmonic in beats
+        per second.
+        """
+        interval = Interval(note1, note2)
+        # check that interval is up, if not, reduce to that case
+        if not interval.up:
+            return -self.get_dev_bps(note2, note1)
+        # get frequencies
+        freq1 = self.get_frequency(note1)
+        freq2 = self.get_frequency(note2)
+        # calculate difference in first harmonic, in all cases
+        if interval.key_distance == 0 and interval.position_distance == 0:
+            # perfect unison
+            return freq2 - freq1
+        elif interval.key_distance == 1 and interval.position_distance == 2:
+            # major second
+            return 7 * freq2 - 8 * freq1
+        elif interval.key_distance == 2 and interval.position_distance == 3:
+            # minor third
+            return 5 * freq2 - 6 * freq1
+        elif interval.key_distance == 2 and interval.position_distance == 4:
+            # major third
+            ratio = 4 * freq2 - 5 * freq1
+        elif interval.key_distance == 3 and interval.position_distance == 5:
+            # perfect fourth
+            ratio = 3 * freq2 - 4 * freq1
+        elif interval.key_distance == 3 and interval.position_distance == 6:
+            # augmented fourth
+            ratio = 7 * freq2 - 10 * freq1
+        elif interval.key_distance == 4 and interval.position_distance == 6:
+            # diminished fifth
+            ratio = 5 * freq2 - 7 * freq1 # (is less than augmented fourth!!)
+        elif interval.key_distance == 4 and interval.position_distance == 7:
+            # perfect fifth
+            ratio = 2 * freq2 - 3 * freq1
+        elif interval.key_distance == 5 and interval.position_distance == 8:
+            # minor sixth
+            ratio = 5 * freq2 - 8 * freq1
+        elif interval.key_distance == 5 and interval.position_distance == 9:
+            # major sixth
+            ratio = 6 * freq2 - 10 * freq1
+        elif interval.key_distance == 5 and interval.position_distance == 9:
+            # minor seventh
+            ratio = 4 * freq2 - 7 * freq1
+        else:
+            raise ValueError("Not a natural interval.")
+        if interval.up:
+            return ratio
+        else:
+            return 1 / ratio
+
+    def get_dev_bpm(self, note1, note2):
+        """Get the difference of the first common harmonic in beats
+        per minute.
+        """
+        return 60 * self.get_dev_bps(note1, note2)
+
+    def __str__(self):
+        """Print information about the temperament.
+
+        >>> temp = Temperament()
+        >>> temp.set_frequency(Note('Eb'), 145.73388203017834)
+        >>> temp.tune_fifths_up(Note('Eb'), Note('G#'))
+        >>> print(temp)
+                   INT/c      DEV/c    DEV/bpm
+                  % 1200                      
+        --------------------------------------
+        A -E      701.96       0.00       0.00
+        E -B      701.96       0.00       0.00
+        B -F#     701.96       0.00       0.00
+        F#-C#     701.96       0.00       0.00
+        C#-G#     701.96       0.00       0.00
+        G#-D#     678.49     -23.46    -477.19
+        Eb-Bb     701.96       0.00       0.00
+        Bb-F      701.96       0.00       0.00
+        F -C      701.96       0.00       0.00
+        C -G      701.96       0.00       0.00
+        G -D      701.96       0.00       0.00
+        D -A      701.96       0.00       0.00
+        <BLANKLINE>
+        """
+        result = (
+            "      {0: >10} {1: >10} {2: >10}\n"
+            .format("INT/c", "DEV/c", "DEV/bpm"))
+        result += (
+            "      {0: >10} {1: >10} {2: >10}\n"
+            .format("% 1200", "", ""))
+        result += (
+            "------{0:->10}-{1:->10}-{2:->10}\n"
+            .format("", "", ""))
+        note = Note('A')
+        fifth = Interval(Note('C'), Note('G'))
+        while True:
+            next_note = note + fifth
+            # note: add 1e-8 to avoid -0.00
+            result += (
+                "{0: <2}-{1: <2} {2: >10.2f} {3: >10.2f} {4: >10.2f}\n"
+                .format(
+                    note, next_note,
+                    self.get_cents(note, next_note) % 1200 + 1e-8,
+                    self.get_dev_cents(note, next_note) + 1e-8,
+                    self.get_dev_bpm(note, next_note) + 1e-8
+                    ))
+            note = self.NOTES[next_note.position]
+            if note == Note('A'):
+                break
+        return result
+
+    def tune_fifth_up(self, note, cents=0):
+        """Tune a fifth, and return the tuned note.
+
+        >>> temp = Temperament()
+        >>> temp.set_frequency(Note('C#'), 300)
+        >>> temp.get_frequency(temp.tune_fifth_up(Note('C#')))
+        450.0
+        >>> temp.get_frequency(temp.tune_fifth_up(Note('G#')))
+        337.5
+        >>> temp.get_frequency(temp.tune_fifth_up(Note('G#'), -20)) # doctest: +ELLIPSIS
+        341.42...
+        """
+        ratio = cents_to_ratio(cents)
+        next_note = note + self.PERFECT_FIFTH
+        if next_note > note:
+            self.set_frequency(next_note,
+                               self.get_frequency(note) * (ratio * 3 / 2))
+        else:
+            self.set_frequency(next_note,
+                               self.get_frequency(note) / (ratio * 4 / 3))
+        return next_note
+
+    def tune_fifths_up(self, note1, note2, cents=0):
+        """Tune fifths going up (or fourths going down), from note1 to note2,
+        such that each fifth has exactly the given deviation in cents.
+
+        >>> # test this with pythagoras
+        >>> temp = Temperament()
+        >>> temp.set_frequency(Note('Eb'),
+        ...                    (415/2)/(3/2)*(4/3)/(3/2)*(4/3)*(4/3)/(3/2))
+        >>> temp.tune_fifths_up(Note('Eb'), Note('G#'))
+        >>> print(2 * temp.get_frequency(Note('A')))
+        415.0
+        """
+        note = note1
+        while note != note2:
+            note = self.tune_fifth_up(note, cents)
+
+    def tune_major_third(self, note1, note2, cents=0):
+        """Tune a major third by tuning four identical fifths, such that the
+        resulting third has the given deviation in cents.
+        """
 
 if __name__ == '__main__':
     import doctest
